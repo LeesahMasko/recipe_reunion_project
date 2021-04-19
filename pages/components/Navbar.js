43a57fba8ccafb15bdca0958c5b515e4d7c061ae
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-04-19 11:40:48 -0700

    adding some components

---
+++ pages/components/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+            <h1>Recipe Reunion</h1>
+            </div>
+            <a> Home</a>
+            <a> My Story</a>
+            <a> My Recipes</a>
+            <a> My Cookbooks</a>
+            <a> Find People</a>
+            <a> Find Foods!</a>
+
+        </nav>
+     );
+}
+
+export default Navbar ;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
+      <Footer />
     </div>
   )
 }
