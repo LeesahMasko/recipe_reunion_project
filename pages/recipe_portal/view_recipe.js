804a0045fb6cf70c63db95dd86e17b412c152c29
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-04-22 15:11:48 -0700

    getting some ideas noted

---
+++ pages/recipe_portal/view_recipe.js
@@ -0,0 +1,8 @@
+// ideas:  One basic page that show the recipe on the right
+// a place where you can see who else in your 'reunion' has snagged that recipe
+// a place where you can upload a video with recipe instructions/how-to's or just talking about the food and memories associated with it
+// a place where you can send a specific recipe to your friend's cookbook
+// a place where you can click "working on this now" to show people that you are actively cooking or "Going to make this tonight"
+// a place to show modifications you've made (versions) to recipes ("I didn't have any milk, I used half&half and it turned out awesome!")
+// a place where you can see what your friends have recently started, planning to start or reccomending
+// maybe tabs??
